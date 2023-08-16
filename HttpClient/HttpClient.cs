4b8a5be7f8de806a2e8798f12cc57ae3cc7d71a5
NVentrelli <nazarenoventrelli@gmail.com>
2023-08-16 13:46:12 -0300

    httpclient added

---
+++ HttpClient/HttpClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace UserPostsAPI.Clients
+{
+    public class HttpClient : IHttpClient
+    {
+        private System.Net.Http.HttpClient _client;
+
+        public HttpClient()
+        {
+            _client = new System.Net.Http.HttpClient();
+        }
+
+        public async Task<HttpResponseMessage> Post(HttpContent httpContent, string url = "")
+        {
+            var response = await _client.PostAsync(url, httpContent);
+
+            return response;
+        }
+    }
+}
---
+++ HttpClient/Interfaces/IHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UserPostsAPI.Clients
+{
+    public interface IHttpClient
+    {
+        public Task<HttpResponseMessage> Post(HttpContent httpContent, string url = "");
+    }
+}
